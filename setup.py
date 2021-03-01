3ab594c5f67df7cd7677af23139e18150683239a
lauracabayol <40888422+lauracabayol@users.noreply.github.com>
2021-03-01 09:46:30 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+# This requires PyTorch, which can not be automatically be installed
+# using pip.
+
+setup(
+    name = "bkgnet",
+    version = "0.1.0",
+    author = "Laura Cabayol",
+    author_email = "lcabayol@ifae.es",
+    description = ("Photometry on PAUCam images with neural networks."),
+    keywords = "astronomy",
+    url = "https://gitlab.pic.es/pau/bkgnet",
+    license="GPLv3",
+    packages=['bkgnet'],
+    install_requires=['numpy', 'pandas'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Astronomy",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+)
